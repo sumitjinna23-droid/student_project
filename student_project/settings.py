2e24e5b0b7e2f21a10c415f70c423d26afc4be47
Sumit <sumitjinna23@example.com>
2026-09-09 14:47:41 +0530

    Add ssl_require=True to Neon database configuration

--- student_project/settings.py
+++ student_project/settings.py
@@ -89,7 +89,8 @@ WSGI_APPLICATION = 'student_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
